379f79b51e85f18225f292337483ed5ffe26f007
kiranchaudhary18 <kiran.chaudhary.cg@gmail.com>
2024-12-04 12:16:20 +0530

    add

---
+++ vite_app/src/Homepage.jsx
@@ -0,0 +1,41 @@
+
+import './style.css'
+
+function HomePage(){
+
+    return (
+        
+      <>  
+
+        <div className="container">
+        
+        <div className="digits" style={{backgroundcolor:'rgb(185, 193, 193)'}}>c</div>
+        <div className="digits" style={{backgroundcolor:'rgb(185, 193, 193)'}}>+/-</div>
+        <div className="digits" style={{backgroundcolor:'rgb(185, 193, 193)'}}>%</div>
+        <div className="digits" style={{backgroundcolor:'orange'}}>/</div>
+        <div className="digits" style={{backgroundcolor:'rgb(81, 86, 86)'}}>7</div>
+        <div className="digits" style={{backgroundcolor:'rgb(81, 86, 86)'}}>8</div>
+        <div className="digits" style={{backgroundcolor:'rgb(81, 86, 86)'}}>9</div>
+        <div className="digits" style={{backgroundcolor:'orange'}}>x</div>
+        <div className="digits" style={{backgroundcolor:'rgb(81, 86, 86)'}}>4</div>
+        <div className="digits" style={{backgroundcolor:'rgb(81, 86, 86)'}}>5</div>
+        <div className="digits" style={{backgroundcolor:'rgb(81, 86, 86)'}}>6</div>
+        <div className="digits" style={{backgroundcolor:'orange'}}>-</div>
+        <div className="digits" style={{backgroundcolor:'rgb(81, 86, 86)'}}>1</div>
+        <div className="digits" style={{backgroundcolor:'rgb(81, 86, 86)'}}>2</div>
+        <div className="digits" style={{backgroundcolor:'rgb(81, 86, 86)'}}>3</div>
+        <div className="digits" style={{backgroundcolor:'orange'}}>+</div>
+        <div className="digits" style={{backgroundcolor:'rgb(81, 86, 86)" id="gap'}}>0</div>
+        <div className="digits" style={{backgroundcolor:'rgb(81, 86, 86)'}}>,</div>
+        <div className="digits" style={{backgroundcolor:'orange'}}>=</div>
+
+        </div>
+
+</>
+          
+      
+      )
+
+}
+
+export default HomePage
